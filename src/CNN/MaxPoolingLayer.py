04f14807586f0fb1f36ffc2cf7b45f780b01bf23
BeanOfChaos <benjarte@ulb.ac.be>
2019-04-24 13:28:59 +0200

    bit of linting

--- src/CNN/MaxPoolingLayer.py
+++ src/CNN/MaxPoolingLayer.py
@@ -17,9 +17,12 @@ class MaxPoolingLayer(Layer):
             partSize :  side size of the square area
         """
         # computing the shape of the result tensor and building it
-        resShape = tuple([int(tensor.shape[0]/partSize), int(tensor.shape[1]/partSize), tensor.shape[2]])
+        resShape = tuple([int(tensor.shape[0]/partSize),
+                          int(tensor.shape[1]/partSize),
+                          tensor.shape[2]])
         res = np.zeros(resShape)
-        # the tensor should be 3 dimensions, the first being the number of the filters used in the previous layer,
+        # the tensor should be 3 dimensions,
+        # the first being the number of the filters used in the previous layer,
         # the second and third being the coordinates on the feature map
         for resi in range(resShape[0]):
             for resj in range(resShape[1]):
@@ -29,28 +32,27 @@ class MaxPoolingLayer(Layer):
                     # computing the max value of the the sub square matrix in coordinates tensi, tensj and
                     # with a sied size equal to partSize
                     res[resi, resj, k] = np.max(tensor[tensi:tensi+partSize, tensj:tensj+partSize, k])
-        return res # TODO return tensor used to learn data
+        return res  # TODO return tensor used to learn data
 
     @staticmethod
     def learnMaxPool(loss, savedData, partSize):
         """
             Learning function of max pooling, it sends back the loss to the neurons chosen in the forward pass
         """
-        previousLayerLoss, sentData = savedData # savedData = (receivedInput, sentData)
+        previousLayerLoss, sentData = savedData  # savedData = (receivedInput, sentData)
         for lossi in range(loss.shape[0]):
             for lossj in range(loss.shape[1]):
                 for k in range(loss.shape[2]):
                     inputi, inputj = lossi*partSize, lossj*partSize
                     # max values sent are used to find their position in the input and replace
                     # them by the loss, other values are replaced by 0
-                    previousLayerLoss[inputi:inputi+partSize, inputj:inputj+partSize, k] = \
-                        np.where(\
-                            previousLayerLoss[inputi:inputi+partSize, inputj:inputj+partSize, k] == sentData[lossi, lossj, k], \
-                            loss[lossi, lossj, k], \
-                            0 \
-                            )
+                    previousLayerLoss[inputi:inputi+partSize, inputj:inputj + partSize, k] = \
+                        np.where(
+                            previousLayerLoss[inputi:inputi+partSize, inputj:inputj + partSize, k] == sentData[lossi, lossj, k],
+                            loss[lossi, lossj, k],
+                            0)
         return previousLayerLoss
-    
+
     def compute(self, tensor):
         """
             Wraps the computation static method
@@ -64,4 +66,6 @@ class MaxPoolingLayer(Layer):
             Wraps the learning static method and update the filters
         """
         if self.isLearning():
-            return MaxPoolingLayer.learnMaxPool(loss, self.getSavedData(), self._partitionSize)
+            return MaxPoolingLayer.learnMaxPool(loss,
+                                                self.getSavedData(),
+                                                self._partitionSize)
