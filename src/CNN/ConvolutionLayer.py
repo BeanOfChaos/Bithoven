1e14681c93f47c991bfbee18477bca3dd8f15ca3
BeanOfChaos <benjarte@ulb.ac.be>
2019-04-25 17:55:31 +0200

    Convolutions fixes
    - Convolutions without padding, coz fuck padding
    - Correct implementation (?)

--- src/CNN/ConvolutionLayer.py
+++ src/CNN/ConvolutionLayer.py
@@ -20,23 +20,22 @@ class ConvolutionLayer(Layer):
             filters : an array of filters (3 dimensional filters)
         """
         # init the resulting feature map
-        featureMap = np.zeros(tuple(list(tensor.shape[:2])+[filters.shape[0]]))
-
-        print(filters.shape, tensor.shape, featureMap.shape)
-
+        truc = list(tensor.shape[:-1])
+        truc = [(truc[i] - filters.shape[i+1]) // (stride)
+                for i in range(len(truc))] + [filters.shape[0]]
+        featureMap = np.zeros(tuple(truc))
+        """
         tensor = np.pad(tensor, ((0, filters.shape[1] - stride),
                         (0, filters.shape[2] - stride), (0, 0)), "constant")
+        """
         for f in range(filters.shape[0]):  # for each 3-dimensional filter
             for i in range(featureMap.shape[0]):  # line i
                 for j in range(featureMap.shape[1]):  # column j
                     # we compute the result of the dot product between:
-                    # (1) the current receptive field
-                    # and (2) the current filter (3 dimensional dot product)
+                    # (1) the current receptive field, and
+                    # (2) the current filter (3 dimensional dot product)
                     featureMap[i][j][f] \
-                            = np.tensordot(tensor[i:i+filters.shape[1],
-                                                  j:j+filters.shape[2], :],
-                                           filters[f],
-                                           axes=([0, 1, 2], [0, 1, 2]))
+                            = np.sum(tensor[i:i+filters.shape[1], j:j+filters.shape[2], :].flatten() * filters[f].flatten()) / filters[0].size
         return featureMap
 
     @staticmethod
--- src/Discriminator.py
+++ src/Discriminator.py
@@ -7,19 +7,20 @@ CHANNEL_NUM = 3
 
 class Discriminator(CNN):
 
-    def __init__(self, isTraining=True):
-        super(Discriminator, self).__init__(isTraining)
+    def __init__(self, isLearning=True):
+        super(Discriminator, self).__init__(isLearning)
 
     def buildNetwork(self):
         self.addConvLayer(np.random.rand(2, 7, 7, CHANNEL_NUM), 0.01)
-        #self.addReluLayer()
+        self.addReluLayer()
         #self.addConvLayer(np.random.rand(4, 7, 7, 8), 0.01)
         #self.addReluLayer()
-        self.addPoolingLayer()
+        self.addPoolingLayer(partitionSize=4)
         #self.addConvLayer(np.random.rand(2, 7, 7, 4), 0.01)
         #self.addReluLayer()
-        self.addPoolingLayer()
-        self.addFullyConnectedLayer(np.random.rand(8192), 0.01)
+        #self.addPoolingLayer()
+        # TODO: express the size of the layer w.r.t. convolution stride size
+        self.addFullyConnectedLayer(np.random.rand(7688), 0.01)
         # do that again
 
         # self.addFullyconnectedLayer(classes = 2) # is / isn't generated music
