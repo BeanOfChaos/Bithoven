1882fc2e1b3bceaf6738b57a39c97fd8844eccd8
BeanOfChaos <benjarte@ulb.ac.be>
2019-04-30 14:36:25 +0200

    various changes
    - utils file containing definition of some general math function
    - flexibility regarding layers activation functions
    - bit of cleanup

--- src/CNN/FullyConnectedLayer.py
+++ src/CNN/FullyConnectedLayer.py
@@ -1,23 +1,18 @@
 import numpy as np
 from CNN.AbstractLayer import Layer
+import CNN.utils as utils
+
 
 class FullyConnectedLayer(Layer):
 
-    def __init__(self, weights, learningRate, isLearning=True):
+    def __init__(self, weights, learningRate, act_f=utils.sigmoid, isLearning=True):
         super(FullyConnectedLayer, self).__init__(isLearning)
+        self._act_f = act_f  # activation function
         self._weights = weights
         self._learningRate = learningRate
 
     @staticmethod
-    def sigmoid(value):
-        """
-        Basic Sigmoid calculation
-        """
-        #return np.exp(value)/(np.exp(value) + 1)
-        return 1/(np.exp(-value) + 1)
-
-    @staticmethod
-    def connect(vector, weights):
+    def connect(vector, weights, act_f):
         """
         Does the dot product between the input vector and the filter.
         Vector is a   1 x n array
@@ -25,14 +20,14 @@ class FullyConnectedLayer(Layer):
         result is a   1 x 1 array for the two nodes of the fully connected layer.
         """
         node = np.dot(vector, weights)
-        result = FullyConnectedLayer.sigmoid(node)
+        result = act_f(node)
         return (node, result)
 
     def squaredError(prediction, actual):
         return 1/2 * np.square(prediction - actual)
 
     @staticmethod
-    def learnFullyConnected(loss, previousLayer, alpha, weights, learningRate):
+    def learnFullyConnected(loss, previousLayer, alpha, weights, learningRate, act_f):
         """
         previousLayer : values of the nodes from the previous layer
         alpha : sum(xi * wi) -> used for derivation
@@ -42,11 +37,13 @@ class FullyConnectedLayer(Layer):
         previousLayerLoss = np.zeros(previousLayer.shape)
         # will be used to compute the updated weightss
         filtersCorrection = np.zeros(weights.shape)
+        print(loss)
         for i in range(weights.shape[0]):  # for i along the height
-            filtersCorrection[i] = loss * previousLayer[i] * np.exp(alpha) / np.square(np.exp(alpha)+1)
-            previousLayerLoss[i] = loss * weights[i] * np.exp(alpha) / np.square(np.exp(alpha)+1)
+            df = act_f(alpha, derivative=True)
+            filtersCorrection[i] = loss * previousLayer[i] * df
+            previousLayerLoss[i] = loss * weights[i] * df
 
-            weights = weights - learningRate * filtersCorrection
+            weights -= learningRate * filtersCorrection
 
         return previousLayerLoss, weights
 
@@ -54,8 +51,10 @@ class FullyConnectedLayer(Layer):
         """
         basic computation function, calls the main function
         """
-        vector = np.reshape(tensor, -1)
-        node, res = FullyConnectedLayer.connect(vector, self._weights)
+        vector = tensor.flatten()
+
+        node, res = FullyConnectedLayer.connect(vector, self._weights, self._act_f)
+        # saves last input and intermediate results
         self.saveData((tensor, node))
         return res
 
@@ -64,7 +63,5 @@ class FullyConnectedLayer(Layer):
         basic learning method, sets some parameters and calls the main function
         """
         previousLayer, alpha = self.getSavedData()
-        previousLayerLoss, self._weights = FullyConnectedLayer.learnFullyConnected(loss, previousLayer.reshape(-1), alpha, self._weights, self._learningRate)
+        previousLayerLoss, self._weights = FullyConnectedLayer.learnFullyConnected(loss, previousLayer.flatten(), alpha, self._weights, self._learningRate, self._act_f)
         return previousLayerLoss.reshape(previousLayer.shape)
-
-		
---
+++ src/CNN/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def sigmoid(value, derivative=False):
+    """
+    Basic Sigmoid calculation
+    """
+    # return np.exp(value)/(np.exp(value) + 1)
+    if derivative:
+        sigm = sigmoid(value)
+        return sigm * (1 - sigm)
+    else:
+        return 1/(np.exp(-value) + 1)
+
+
+def reLu(x, derivative=False):
+    """Rectified linear unit (ReLu) function
+    """
+    if derivative:
+        return x > 0  # True == 1 and False == 0
+    else:
+        return x if x > 0 else 0
