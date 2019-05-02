3ea7b0ab1ae7506a841df1c1fcd04f28e17245b7
BeanOfChaos <benjarte@ulb.ac.be>
2019-05-02 10:28:20 +0200

    uncommented layers + correct loss (maybe?)

--- src/Discriminator.py
+++ src/Discriminator.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle
 
 CHANNEL_NUM = 3
-LEARNING_RATE = 0.05
+LEARNING_RATE = 1.
+
 
 class Discriminator(CNN):
 
@@ -12,16 +13,16 @@ class Discriminator(CNN):
 
     def buildNetwork(self):
         self.addConvLayer(np.random.rand(2, 7, 7, CHANNEL_NUM), LEARNING_RATE)
-        self.addReluLayer()
-        #self.addConvLayer(np.random.rand(4, 7, 7, 8), 0.01)
-        #self.addReluLayer()
+        # self.addReluLayer()
+        self.addConvLayer(np.random.rand(4, 7, 7, 2), LEARNING_RATE)
+        # self.addReluLayer()
         self.addPoolingLayer(partitionSize=4)
-        #self.addConvLayer(np.random.rand(2, 7, 7, 4), 0.01)
-        #self.addReluLayer()
-        #self.addPoolingLayer()
+        self.addConvLayer(np.random.rand(2, 7, 7, 4), LEARNING_RATE)
+        self.addReluLayer()
+        self.addPoolingLayer()
         # TODO: express the size of the layer w.r.t. convolution stride size
         # instead of hard coding it
-        self.addFullyConnectedLayer(np.random.rand(7688), LEARNING_RATE)
+        self.addFullyConnectedLayer(np.random.rand(1352), LEARNING_RATE)
         # do that again
 
         # self.addFullyconnectedLayer(classes = 2) # is / isn't generated music
